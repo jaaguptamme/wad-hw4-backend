cbfdd2c5531a6da3eb5879b71dc7b4c32aa9e2b7
Andres <andresalumets@hotmail.com>
2023-12-06 19:18:56 +0200

    Added deleting posts and populating single post pages.

--- server.js
+++ server.js
@@ -132,11 +132,25 @@ app.post('/posts', async(req, res) => {
         const post = req.body;
         const newpost = await pool.query(
             "INSERT INTO posts(body) values ($1)    RETURNING*", [post.body]
-            // $1, $2, $3 are mapped to the first, second and third element of the passed array (post.title, post.body, post.urllink)
             // The RETURNING keyword in PostgreSQL allows returning a value from the insert or update statement.
             // using "*" after the RETURNING keyword in PostgreSQL, will return everything
         );
         res.json(newpost);
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
+
+app.delete('/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        //const post = req.body; // we do not need a body for a delete request
+        console.log("delete a post request has arrived");
+        console.log(id)
+        const deletepost = await pool.query(
+            "DELETE FROM posts WHERE id = $1", [id]
+        );
+        res.json(deletepost);
     } catch (err) {
         console.error(err.message);
     }
@@ -153,3 +167,22 @@ app.get('/posts', async(req, res) => {
         res.status(401).json({ error: error.message });
     }
 });
+
+app.get('/posts/:id', async(req, res) => {
+    try {
+        console.log("get a post with route parameter request has arrived");
+        // The req.params property is an object containing properties mapped to the named route "parameters". 
+        // For example, if you have the route /posts/:id, then the "id" property is available as req.params.id.
+        const { id } = req.params; // assigning all route "parameters" to the id "object"
+
+        const posts = await pool.query( // pool.query runs a single query on the database.
+            //$1 is mapped to the first element of { id } (which is just the value of id). 
+            "SELECT * FROM posts WHERE id = $1", [id]
+        );
+        res.json(posts.rows[0]); // we already know that the row array contains a single element, and here we are trying to access it
+        // The res.json() function sends a JSON response. 
+        // This method sends a response (with the correct content-type) that is the parameter converted to a JSON string using the JSON.stringify() method.
+    } catch (err) {
+        console.error(err.message);
+    }
+}); 
